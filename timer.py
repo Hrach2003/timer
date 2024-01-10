4d454091aad55f9640b22d5946c35fc39abb12ae
Hrach2003 <155984992+Hrach2003@users.noreply.github.com>
2024-01-10 13:31:27 +0400

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,22 +1,22 @@
-# import time
-# def start_stopwatch():
-#     start_time = time.time()
-#     input("Press enter to stop.")
-#     end_time = time.time()
-#     elapse_time = end_time - start_time
-#     print(elapse_time)
-# print("Welcome to this python stopwatch!")
-#
-# while True:
-#     print("\noptions: ")
-#     print("1. Start working")
-#     print("2. Exit")
-#     choice = input("Enter your choice (1-2): ")
-#
-#     if choice == '1':
-#         print(f"Elapsed Time: {start_stopwatch()}")
-#     elif choice == '2':
-#         print("Exiting the stopwatch. Goodbye!")
-#         break
-#     else:
-#         print("Invalid choice. Please enter 1 or 2")
+import time
+def start_stopwatch():
+    start_time = time.time()
+    input("Press enter to stop.")
+    end_time = time.time()
+    elapse_time = end_time - start_time
+    print(elapse_time)
+print("Welcome to this python stopwatch!")
+
+while True:
+    print("\noptions: ")
+    print("1. Start working")
+    print("2. Exit")
+    choice = input("Enter your choice (1-2): ")
+
+    if choice == '1':
+        print(f"Elapsed Time: {start_stopwatch()}")
+    elif choice == '2':
+        print("Exiting the stopwatch. Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please enter 1 or 2")
